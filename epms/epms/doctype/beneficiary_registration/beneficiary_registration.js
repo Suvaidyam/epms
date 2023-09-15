fd033216f5919242edad8bba1a0faeaa076daaa6
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-09-15 17:32:09 +0530

    add

--- epms/epms/doctype/beneficiary_registration/beneficiary_registration.js
+++ epms/epms/doctype/beneficiary_registration/beneficiary_registration.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Beneficiary Registration", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Beneficiary Registration", {
+	refresh(frm) {
+        frm.fields_dict["district"].get_query = function (doc) {
+            return {
+              filters: {
+                State: "please select state first",
+              },
+            };
+          }
+	},
+    
+});
