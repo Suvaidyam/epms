81bb4d2df076602dc4d528cc8fb86be02dcd14a9
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-09-16 15:52:39 +0530

    add address dependent dropdowns

--- epms/epms/doctype/beneficiary/beneficiary.js
+++ epms/epms/doctype/beneficiary/beneficiary.js
@@ -3,6 +3,7 @@
 
 frappe.ui.form.on("Beneficiary", {
   refresh(frm) {
+  //  ID PROOF SECTION LOGIC FOR SHOW AND HIDE SECTION
     var id_section = frm.get_field('id_section');
     if (frm.doc.do_you_have_id_document === 'Yes') {
         id_section.df.hidden = 0;
@@ -11,29 +12,46 @@ frappe.ui.form.on("Beneficiary", {
         id_section.df.hidden = 1;
         id_section.refresh();
     }
+// CURRENT RESIDENT DEPENDENT DROPDOWNS LOGICS
+        frm.fields_dict["current_district"].get_query = function (doc) {
+            return {
+              filters: {
+                State: "please select Current state",
+              },
+            };
+          }
+        frm.fields_dict["current_block"].get_query = function (doc) {
+            return {
+              filters: {
+                District: "please select Current district",
+              },
+            };
+          }
+//  ORIGIN RESIDENT DEPENDENT DROPDOWNS LOGICS
+                frm.fields_dict["district_of_origin"].get_query = function (doc) {
+            return {
+              filters: {
+                State: "please select state of origin",
+              },
+            };
+          }
+        frm.fields_dict["current_block"].get_query = function (doc) {
+            return {
+              filters: {
+                District: "please select district of origin",
+              },
+            };
+          }
 
-        frm.fields_dict["district_of_origin" && "current_district"].get_query = function (doc) {
-            return {
-              filters: {
-                State: "please select state first",
-              },
-            };
-          }
-        frm.fields_dict["block_of_origin" && "current_block"].get_query = function (doc) {
-            return {
-              filters: {
-                District: "please select district first",
-              },
-            };
-          }
+// DEFULTS DATE SET 
           frm.set_value('date_of_visit', frappe.datetime.get_today());
-          // frm.set_value('contact_number', '+91')
+
 	},
   state_of_origin: function(frm){
     frm.fields_dict["district_of_origin"].get_query = function (doc) {
       return {
         filters: {
-          State: frm.doc.state,
+          State: frm.doc.state_of_origin,
         },
       };
     }
@@ -47,6 +65,25 @@ frappe.ui.form.on("Beneficiary", {
       };
     }
   },
+  current_state: function(frm){
+    frm.fields_dict["current_district"].get_query = function (doc) {
+      return {
+        filters: {
+          State: frm.doc.current_state,
+        },
+      };
+    }
+  },
+  current_district: function(frm){
+    frm.fields_dict["current_block"].get_query = function (doc) {
+      return {
+        filters: {
+          District: frm.doc.current_district,
+        },
+      };
+    }
+  },
+
   do_you_have_id_document: function(frm){
       var id_section = frm.get_field('id_section');
         if (frm.doc.do_you_have_id_document === 'Yes') {
