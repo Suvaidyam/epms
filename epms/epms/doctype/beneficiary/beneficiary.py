cd5d9e62deab40194822b751dd96e32fef88e825
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-09-19 11:45:58 +0530

    add auto up date of beneficary

--- epms/epms/doctype/beneficiary/beneficiary.py
+++ epms/epms/doctype/beneficiary/beneficiary.py
@@ -13,14 +13,36 @@ class Beneficiary(Document):
 			family_doc = frappe.new_doc("Family")
 			family_doc.head_of_family = beneficiary.name
 			family_doc.name_of_parents = beneficiary.name_of_the_beneficiary
-			family_doc.name_of_parents = beneficiary.contact_number
+			family_doc.contact_number = beneficiary.contact_number
 			family_doc.insert()
 			# update current beneficery
 			beneficiary.family = family_doc.name
 			beneficiary.save()
 		else:
 			print("CREATING CHILD BENEFICARY")
+	def on_update(self):
+		beneficiary = frappe.get_doc("Beneficiary" , self.name)
+		if(beneficiary.are_you_parents == 1):
+			family_doc_name = frappe.get_list("Family",
+        	filters={'head_of_family': self.name},
+        	fields=["name"])
+			if(family_doc_name):
+				family_doc = frappe.get_doc("Family", family_doc_name[0].name)
+				family_doc.name_of_parents = beneficiary.name_of_the_beneficiary
+				family_doc.contact_number = beneficiary.contact_number
+				family_doc.save()
+			else:
+				family_doc = frappe.new_doc("Family")
+				family_doc.head_of_family = beneficiary.name
+				family_doc.name_of_parents = beneficiary.name_of_the_beneficiary
+				family_doc.contact_number = beneficiary.contact_number
+				family_doc.insert()
+				# update current beneficery to family
+				beneficiary.family = family_doc.name
+				beneficiary.save()
+				frappe.msgprint("New Beneficary Update As a Head of Family")
+		else:
+			print("THis is beneficary is not a parent ")
+			# frappe.msgprint("update is parent not called")
 
-	def after_insert(self):
-		print("hello")
 
