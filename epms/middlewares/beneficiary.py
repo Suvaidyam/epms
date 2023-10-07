3f3a2ee85aa52db47122e3eb4ac1edd12b176fb2
Ubuntu <ubuntu@ip-172-31-14-203.ap-south-1.compute.internal>
2023-10-07 02:45:27 +0000

    hooks

--- epms/middlewares/beneficiary.py
+++ epms/middlewares/beneficiary.py
@@ -1,7 +1,8 @@
+import frappe
 def list_query(user):
     if not user:
         user = frappe.session.user
-
+    print(user)
     # todos that belong to user or assigned by user
     return ""
     #return "(`tabBeneficiary`.csc = 'CSC-S07097-002')"
