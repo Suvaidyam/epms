0751277e6b70f828555b1af483590d6667c31719
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-17 10:41:52 +0530

    add tow doctype

---
+++ epms/epms/doctype/data_overview/test_data_overview.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDataOverview(FrappeTestCase):
+	pass
