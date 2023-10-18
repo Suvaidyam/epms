3ad720dd776d58f9c4cb3baaa3600cfc36181078
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-18 22:34:30 +0530

    correct session and add reports

--- epms/middlewares/family.py
+++ epms/middlewares/family.py
@@ -1,10 +1,11 @@
 import frappe
+from epms.utils.cache import Cache
 def list_query(user):
     if not user:
         user = frappe.session.user
         
     if "MIS executive" in frappe.get_roles(user) and ("Administrator" not in frappe.get_roles(user)):
-        value = frappe.cache().get_value("csc-"+user)
+        value = Cache.get_csc()
         # frappe.msgprint("csc"+(value))
         return """(`tabPrimary Member`.csc = '{0}')""".format(value)
     # System Managers have full access
