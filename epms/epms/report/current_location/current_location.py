d967142691b9d5431c5e823fa1a2e527a591c6f2
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-19 12:30:43 +0530

    add current location

---
+++ epms/epms/report/current_location/current_location.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+	frappe.errprint(filters)
+	columns = [
+		{
+		"fieldname":"education",
+		"label":"Education",
+		"fieldtype":"Data",
+		"width":400
+		},
+		{
+		"fieldname":"count",
+		"label":"Count",
+		"fieldtype":"int",
+		"width":200
+		}
+	]
+
+
+	new_filters =None
+	education = frappe.get_all("Beneficiary",
+	filters=new_filters,
+	fields=["education as education",'count(name) as count'], 
+	group_by='education')
+	print("education", education)
+	data = education
+	return columns, data
