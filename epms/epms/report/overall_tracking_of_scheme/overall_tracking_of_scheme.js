ad4925a05c85e18e03a972302e43d67be735cb8c
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-20 18:07:28 +0530

    add tracking of occupationa by cast

---
+++ epms/epms/report/overall_tracking_of_scheme/overall_tracking_of_scheme.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
+// For license information, please see license.txt
+
+let filters = [
+	{
+		"fieldname": "from_date",
+		"fieldtype": "Date",
+		"label": "From Date",
+	},
+	{
+		"fieldname": "to_date",
+		"fieldtype": "Date",
+		"label": "To Date"
+	},
+	{
+		"fieldname": "to_date",
+		"fieldtype": "Date",
+		"label": "To Date"
+	}
+];
+if (true) {
+	filters.push({
+		"fieldname": "csc",
+		"fieldtype": "Link",
+		"label": "CSC",
+		"options": "CSC"
+	})
+}
+frappe.query_reports["Overall tracking of scheme"] = {
+	"filters": filters
+};
---
+++ epms/epms/report/overall_tracking_of_scheme/overall_tracking_of_scheme.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
