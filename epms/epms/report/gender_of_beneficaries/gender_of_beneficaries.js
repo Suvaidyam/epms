d20fca710bafef47937d7ff912d219c8499875bf
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-21 10:13:00 +0530

    add csc filter to gender composition

--- epms/epms/report/gender_of_beneficaries/gender_of_beneficaries.js
+++ epms/epms/report/gender_of_beneficaries/gender_of_beneficaries.js
@@ -1,17 +1,26 @@
 // Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
 // For license information, please see license.txt
-
+let filters = [
+	{
+		"fieldname": "from_date",
+		"fieldtype": "Date",
+		"label": "From Date",
+	},
+	{
+		"fieldname": "to_date",
+		"fieldtype": "Date",
+		"label": "To Date"
+	}
+	
+];
+if (!frappe.user_roles.includes("MIS executive")) {
+	filters.push({
+		"fieldname": "csc",
+		"fieldtype": "Link",
+		"label": "CSC",
+		"options": "CSC"
+	})
+}
 frappe.query_reports["Gender of Beneficaries"] = {
-	"filters": [
-		{
-			"fieldname":"from_date",
-			"fieldtype":"Date",
-			"label":"From Date",
-		},
-		{
-			"fieldname":"to_date",
-			"fieldtype":"Date",
-			"label":"To Date"
-		}
-	]
+	"filters": filters
 };
