e0897bc43113498a01bfdd5ec53f193c11e29296
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-21 10:21:31 +0530

    add filter of csc to education details

--- epms/epms/report/education_of_beneficiary/education_of_beneficiary.js
+++ epms/epms/report/education_of_beneficiary/education_of_beneficiary.js
@@ -1,17 +1,27 @@
 // Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
 // For license information, please see license.txt
 
+let filters = [
+	{
+		"fieldname": "from_date",
+		"fieldtype": "Date",
+		"label": "From Date",
+	},
+	{
+		"fieldname": "to_date",
+		"fieldtype": "Date",
+		"label": "To Date"
+	}
+	
+];
+if (!frappe.user_roles.includes("MIS executive")) {
+	filters.push({
+		"fieldname": "csc",
+		"fieldtype": "Link",
+		"label": "CSC",
+		"options": "CSC"
+	})
+}
 frappe.query_reports["Education of Beneficiary"] = {
-	"filters": [
-		{
-			"fieldname":"from_date",
-			"fieldtype":"Date",
-			"label":"From Date",
-		},
-		{
-			"fieldname":"to_date",
-			"fieldtype":"Date",
-			"label":"To Date"
-		}
-	]
+	"filters": filters
 };
