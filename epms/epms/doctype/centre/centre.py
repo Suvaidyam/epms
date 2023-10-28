eebe63156bffa08a24a536f78bcf0db3bc36817e
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-10-28 10:54:45 +0530

    Add changes

--- epms/epms/doctype/centre/centre.py
+++ epms/epms/doctype/centre/centre.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class CSC(Document):
+class Centre(Document):
 	pass
--- epms/epms/doctype/centre/test_centre.py
+++ epms/epms/doctype/centre/test_centre.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestCSC(FrappeTestCase):
+class TestCentre(FrappeTestCase):
 	pass
