44db8c39bdedbe0be366bb22bb42cd4d13437ad5
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-12 09:09:39 +0530

    add changes

--- epms/epms/report/current_location/current_location.js
+++ epms/epms/report/current_location/current_location.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2023, Management System for Agrasarteach@suvaidyam.com and contributors
 // For license information, please see license.txt
-
+if (!frappe.user_roles.includes("MIS executive") || frappe.user_roles.includes("Administrator")) {
+	setTimeout(()=>{[...document.querySelectorAll("[data-label='Edit']"),...document.querySelectorAll("[data-action='action-edit']")].forEach(e=> e.remove())},500)
+}
 var filters = [
 	{
 		"fieldname": "from_date",
