b123925ea3642c95854dcb54ddc347e33e7eed06
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-16 21:03:32 +0530

    add changes

--- epms/epms/doctype/beneficiary/beneficiary.js
+++ epms/epms/doctype/beneficiary/beneficiary.js
@@ -670,7 +670,7 @@ frappe.ui.form.on('Follow Up Child', {
     let supports = frm.doc.support_table.filter(f => f.specific_support_type == row.support_name);
     let latestSupport = supports.length ? supports[supports.length - 1] : null;
     if (row.follow_up_with != "Beneficiary" && latestSupport.application_submitted == "Yes") {
-      frm.fields_dict.followup_table.grid.update_docfield_property("follow_up_status", "options", ["Under process", "Additional info required", "Completed", "Rejected"]);
+      frm.fields_dict.followup_table.grid.update_docfield_property("follow_up_status", "options", ["Not reachable" ,"Under process", "Additional info required", "Completed", "Rejected"]);
     }else if(row.follow_up_with == "Beneficiary" && latestSupport.application_submitted == "Yes"){
       frm.fields_dict.followup_table.grid.update_docfield_property("follow_up_status", "options", ["Not reachable", "Under process", "Additional info required", "Completed", "Rejected"]);
     }else if(row.follow_up_with == "Beneficiary" && latestSupport.application_submitted == "No"){
