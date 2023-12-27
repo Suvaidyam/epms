1ed95b861b0d5394d0218a587106e7c06e7b5b5c
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2023-12-27 07:57:17 +0530

    add options for admin to create locations

--- epms/epms/doctype/current_location/current_location.py
+++ epms/epms/doctype/current_location/current_location.py
@@ -3,7 +3,11 @@
 
 # import frappe
 from frappe.model.document import Document
-
+from epms.utils.cache import Cache
 
 class Currentlocation(Document):
-	pass
+	def validate(self):
+		if not self.csc:
+			self.csc = Cache.get_csc()
+	# def after_insert(self):
+	# 	''
